8b11e4f4eee8a771ff03264be5d622762503501b
ripsivis <onurankut123@gmail.com>
2023-01-07 21:25:49 +0300

    Remove unnecessary part

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -7,7 +7,7 @@ for xml in "$DIR0"/*.xml "$DIR1"/*.xml; do
   if [ -f $xml ] && grep -q "$VAR0" $xml; then
     XMLDIR="$MODDIR/system/$(dirname $xml)"
     mkdir -p "$XMLDIR"
-    [ $(ls -A "$XMLDIR") ] && rm -rf "$XMLDIR"/*
+    rm -rf "$XMLDIR"/*
     cp -f $xml "$XMLDIR"
     sed -i s/"$VAR0"'>true<'/"$VAR0"'>false<'/ "$XMLDIR/$(basename $xml)"
   fi
